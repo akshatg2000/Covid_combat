03293fd7004de008858d0686970c7955d8247956
Swarup <22m2102@iitb.ac.in>
2022-09-28 15:53:23 +0530

    Basic setup

--- main.py
+++ main.py
@@ -1,124 +1,137 @@
-import pygame, sys, os, math, random
-
-COLOR_RED = (255, 0, 0)
+# Bullet range is 10 * length_of_grid_size
+# You can shoot a Coronavirus and gain 10 points
+# You can shoot trees without any effect
+# You can shoot infected human beings and lose 100 points
+# You can shoot healthy human beings and game is over
+# You can quickly wear a mask to save yourself by spending 100 points for 10 seconds
+# You can get infected(i.e. a Coronavirus touches you)
+# Infected human beings recover after 14 seconds if no further coronavirus touches him
+# If 3 coronaviruses are alive within you simultaneously, You die and game is over
+# Same rules of death is applicable for all human beings
+# Infected human beings spread coronavirus at a certain rate(say 3 viruses per second)
+# Viruses spread away from the source
+# If points drop below 0, game over
 
 # TODO: Create graphics for CoronaVirus, Tree, Infected Human Beings, Healthy Human Beings, Bullet
 # TODO: Create sounds for firing, infecting, dying, recovering
 
-GRIDE_SIZE = 40
+import pygame, sys, os, math, random
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRID_SIDE = 40
+UPDATE_FREQUENCY = 30
 
 class Tree:
     def __init__(self):
-        ## Load image
         pass
 
 class CoronaVirus:
     def __init__(self):
         pass
 
-class HumanBeing:
+class Human:
     def __init__(self):
-        self.is_infected = False
+        self.infected = False
 
-class You(HumanBeing):
+class Player(Human):
     def __init__(self):
         self.points = 0
-
-class Bullet:
-    ## Bullets will follow projectile motion
-    def __init__(self):
-        self.xSpeed = 10
-        self.ySpeed = 10
-        self.zSpeed = 0
-        self.x = 10
-        self.y = 10
-        self.z = 10
+        self.x = 1
+        self.y = 1
+        self.image = pygame.transform.scale(pygame.image.load("images/player.png"), (GRID_SIDE, GRID_SIDE))
 
     def update(self):
-        self.zSpeed += 0.2 # We take g=2, will adjust later
-        self.x += self.xSpeed
-        self.y += self.ySpeed
-        self.z += self.zSpeed
-        if self.z > 0:
-            pygame.draw.rect(pygame.display.get_surface(), (0, 255, 0), (self.x, self.z, 3, 3))
-        # Check collision
+        pygame.display.get_surface().blit(self.image, (GRID_SIDE * self.x, GRID_SIDE * self.y))
 
-class COVID_Combat:
-    ## Rules:
-    ## Bullet range is 10 * length_of_grid_size
-    ## You can shoot a Coronavirus and gain 10 points
-    ## You can shoot trees without any effect
-    ## You can shoot infected human beings and lose 100 points
-    ## You can shoot healthy human beings and game is over
-    ## You can quickly wear a mask to save yourself by spending 100 points for 10 seconds
-    ## You can get infected(i.e. a Coronavirus touches you)
-    ## Infected human beings recover after 14 seconds if no further coronavirus touches him
-    ## If 3 coronaviruses are alive within you simultaneously, You die and game is over
-    ## Same rules of death is applicable for all human beings
-    ## Infected human beings spread coronavirus at a certain rate(say 3 viruses per second)
-    ## Viruses spread away from the source
-    ## If points drop below 0, game over
+    def change_position(self, delta_position):
+        self.x += delta_position[0]
+        self.y += delta_position[1]
+
+class Bullet:
     def __init__(self):
-        self.battle_field = [
-            ## 0 empty space, 1 tree, 2 infected human, 3 healthy human, 4 coronavirus
+        self.x = 0
+        self.y = 0
+        self.x_velocity = 2
+        self.y_velocity = 2
+
+    def update(self):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+class Battlefield:
+    def __init__(self):
+        self.image1 = pygame.transform.scale(pygame.image.load("images/g1.png"), (GRID_SIDE, GRID_SIDE))
+        self.image2 = pygame.transform.scale(pygame.image.load("images/g2.png"), (GRID_SIDE, GRID_SIDE))
+        self.grid = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1],
+            [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+            [1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ]
 
+    def update(self):
+        for i, row in enumerate(self.grid):
+            for j, color in enumerate(row):
+                image = self.image1
+                if (i + j) % 2:
+                    image = self.image2
+                if (color != 0):
+                    pygame.display.get_surface().blit(image, (GRID_SIDE * i, GRID_SIDE * j))
+
+class COVID_Combat:
+    def __init__(self):
+        self.battlefield = Battlefield()
+        self.player = Player()
+
     def run(self):
-        # pygame.time.Clock().tick(60)
-        running = True
-        display_update_count = 0
-        bullet = Bullet()
-        while running:
+        pygame.time.Clock().tick(UPDATE_FREQUENCY)
+        while True:
+            pygame.display.get_surface().fill((0, 0, 0))
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     pygame.quit()
                     sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    print("Other key pressed")
-            for i, row in enumerate(self.battle_field):
-                for j, color in enumerate(row):
-                    COLOR = COLOR_RED
-                    if j % 2:
-                        COLOR = (0, 255, 0)
-                    if (color != 0):
-                        pygame.draw.rect(pygame.display.get_surface(), COLOR, ((i * GRIDE_SIZE, j * GRIDE_SIZE, GRIDE_SIZE, GRIDE_SIZE)))
-            bullet.update()
-            ## TODO: Currently display is getting updated very rapidly, explore pygame to reduce frequency
-            ## This will cause flikering later
-            display_update_count += 1
-            print("Display updating " + str(display_update_count))
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.player.change_position((1, 0))
+                    elif event.key == pygame.K_LEFT:
+                        self.player.change_position((-1, 0))
+                    elif event.key == pygame.K_UP:
+                        self.player.change_position((0, -1))
+                    elif event.key == pygame.K_DOWN:
+                        self.player.change_position((0, 1))
+            self.battlefield.update()
+            self.player.update()
             pygame.display.flip()
 
 def main():
